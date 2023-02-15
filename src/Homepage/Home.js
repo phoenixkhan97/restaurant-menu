f5932b8d002bc88a580933d751a5d4226de45c85
phoenix <sahidankhan7991@gmail.com>
2023-02-15 12:32:46 -0600

    css in the works

--- src/Homepage/Home.js
+++ src/Homepage/Home.js
@@ -1,19 +1,17 @@
 const home = ()=>{
 
 return(
-    <div>
         <div className="a">
             <div className="b">
                 <h1>Jacie's Cafe</h1>
-                <p></p>
+                <button id="btn" className="menu">Menu</button>
+                <button id="btn"  className="checkout">Checkout</button>
             </div>
-    <div>
+            {/* <div id="food">
+            </div> */}
         
     </div>
-    </div>
 
-
-    </div>
 )
 
 }
