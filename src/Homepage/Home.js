c49622e4fc39622509990a9c4b72fcd84e1d84e5
phoenix <sahidankhan7991@gmail.com>
2023-02-15 11:42:54 -0600

    first commit

---
+++ src/Homepage/Home.js
@@ -0,0 +1,21 @@
+const home = ()=>{
+
+return(
+    <div>
+        <div className="a">
+            <div className="b">
+                <h1>Jacie's Cafe</h1>
+                <p></p>
+            </div>
+    <div>
+        
+    </div>
+    </div>
+
+
+    </div>
+)
+
+}
+
+export default home
